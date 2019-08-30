7ad03337682f3878a794f1ba0c3a54996690a4d3
Jim Rafuse <jimrafuse@users.noreply.github.com>
2019-08-30 08:12:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# README
+
+I would advise you to not use this tap. See below.
+
+- ghostscript-jimrafuse: depends on fontconfig
+- imagemagick@6-jimrafuse: depends on fontconfig, ghostscript-jimrafuse, and **linked into home**
+- ruby-jimrafuse: **linked into home**
+- xpdf-jimrafuse: installs the tools only (`pdfimages`, `pdfinfo`, etc)
---
+++ imagemagick@6-jimrafuse.rb
@@ -0,0 +1,73 @@
+class ImagemagickAT6Jimrafuse < Formula
+  desc "Tools and libraries to manipulate images in many formats"
+  homepage "https://www.imagemagick.org/"
+  # Please always keep the Homebrew mirror as the primary URL as the
+  # ImageMagick site removes tarballs regularly which means we get issues
+  # unnecessarily and older versions of the formula are broken.
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406--6.9.10-62.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.10-62.tar.xz"
+  sha256 "f2e63d43b8e249aa7edd54cd2f841035d3223c5fdbbc812f13420b270c9687d6"
+  head "https://github.com/imagemagick/imagemagick6.git"
+
+  # bottle do
+  #   sha256 "96b2663ac78393c9a965512198425bf3586e60979461c5f009df81757c881134" => :mojave
+  #   sha256 "6a995e000ad38c34f68b628fc9f73ba7aad1626fc49267a7a8ce1f76ce6d4a00" => :high_sierra
+  #   sha256 "e7becf5e2b376c55b72a7fb836a4768ff4bdef7012ff07556351dc040579d878" => :sierra
+  # end
+
+  # keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "ghostscript-jimrafuse"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "little-cms2"
+  depends_on "openjpeg"
+  depends_on "webp"
+  depends_on "xz"
+
+  skip_clean :la
+
+  def install
+    args = %W[
+      --disable-osx-universal-binary
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --disable-opencl
+      --disable-openmp
+      --enable-shared
+      --enable-static
+      --with-fontconfig
+      --with-freetype=yes
+      --with-modules
+      --with-webp=yes
+      --with-openjp2
+      --with-gslib
+      --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+      --without-fftw
+      --without-pango
+      --without-x
+      --without-wmf
+    ]
+
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+    # Check support for recommended features and delegates.
+    features = shell_output("#{bin}/convert -version")
+    %w[Modules freetype jpeg png tiff].each do |feature|
+      assert_match feature, features
+    end
+  end
+end

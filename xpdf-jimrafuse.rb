6fb35616c3b47034b3a223dab2067930d73172dd
Jim Rafuse <jimrafuse@users.noreply.github.com>
2019-10-21 08:51:36 -0400

    Update Ghostscript

--- xpdf-jimrafuse.rb
+++ xpdf-jimrafuse.rb
@@ -1,22 +1,25 @@
 class XpdfJimrafuse < Formula
-    desc "PDF tools"
-    homepage "http://www.foolabs.com/xpdf/"
-    url "https://xpdfreader-dl.s3.amazonaws.com/xpdf-4.01.01.tar.gz"
-    sha256 "ba550c7d3e4f73b1833cfcdcd9dbe39849dd0cd459b6774c4ecdfeca993204a4"
+  desc "PDF tools"
+  homepage "http://www.foolabs.com/xpdf/"
 
-    depends_on "cmake" => :build
-    depends_on "freetype"
+  # JR: Cannot update to 4.02 as Qt requirement is baked in somewhere.
 
-    conflicts_with "pdf2image", "poppler",
-      :because => "xpdf, pdf2image, and poppler install conflicting executables"
+  url "https://xpdfreader-dl.s3.amazonaws.com/xpdf-4.01.01.tar.gz"
+  sha256 "ba550c7d3e4f73b1833cfcdcd9dbe39849dd0cd459b6774c4ecdfeca993204a4"
 
-    def install
-      system "cmake", ".", *std_cmake_args
-      system "make", "install"
-    end
+  depends_on "cmake" => :build
+  depends_on "freetype"
 
-    test do
-      cp test_fixtures("test.pdf"), testpath
-      assert_match "Pages:", shell_output("#{bin}/pdfinfo #{testpath}/test.pdf")
-    end
+  conflicts_with "pdf2image", "poppler",
+    :because => "xpdf, pdf2image, and poppler install conflicting executables"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
   end
+
+  test do
+    cp test_fixtures("test.pdf"), testpath
+    assert_match "Pages:", shell_output("#{bin}/pdfinfo #{testpath}/test.pdf")
+  end
+end

87f894e5e03e5ca8adfc4ae176ba1f25eafc6f42
Jim Rafuse <jimrafuse@users.noreply.github.com>
2020-04-20 17:10:43 -0400

    ImageMagick 6.9.11-7

--- imagemagick@6-jimrafuse.rb
+++ imagemagick@6-jimrafuse.rb
@@ -4,8 +4,8 @@ class ImagemagickAT6Jimrafuse < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-6.tar.xz"
-  sha256 "fc7405ad33e50f97c7961b0ca548e9fbf4bf7cdb68e36b0266a9eead0244ecef"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-7.tar.xz"
+  sha256 "1a9935b78e3430a4a031602838c4b424e6d6e9420058d1448172a3b8ff9995d5"
   head "https://github.com/imagemagick/imagemagick6.git"
 
   depends_on "pkg-config" => :build
